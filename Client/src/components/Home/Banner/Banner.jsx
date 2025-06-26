3808c5ae927c9a8081b22339059e3d3b43639a49
Adeesh-bode <bodead@rknec.edu>
2025-06-26 13:54:07 +0530

    latest checkpoint commit

--- Client/src/components/Home/Banner/Banner.jsx
+++ Client/src/components/Home/Banner/Banner.jsx
@@ -1,23 +1,26 @@
 import "./Banner.scss";
 import BannerImg from "../../../assets/banner-img.png";
 const Banner = () => {
-    return <div className="hero-banner" >
-        <div className="content">
-            <div className="text-content">
-                <h1>SALES</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Pariatur dolor voluptatum sapiente inventore us, 
-                    alaecati atque?
-                </p>
-                <div className="ctas">
-                    <div className="banner-cta">Read More</div>
-                    <div className="banner-cta v2">Shop Now</div>
-                </div>
-            </div>
-            <img alt="" src={BannerImg} className="banner-img" />
+  return (
+    <div className="hero-banner">
+      <div className="content">
+        <div className="text-content">
+          <h1>Cruise with Boat</h1>
+          <p>
+            Welcome to ReactStore - your ultimate destination for exclusive Boat
+            electronics deals! Discover unbeatable discounts on the latest audio
+            gear, smartwatches, and more. Don&apos;t miss out - shop now and upgrade
+            your lifestyle with cutting-edge tech!
+          </p>
+          <div className="ctas">
+            <div className="banner-cta">Read More</div>
+            <div className="banner-cta v2">Shop Now</div>
+          </div>
         </div>
-    </div>;
+        <img alt="" src={BannerImg} className="banner-img" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
